71f25ec6c02c9c9f0802d449c9bdd4118f10d94e
Aaron <64151135+aaronp18@users.noreply.github.com>
2020-05-31 16:44:04 +0100

    Added dungeon generation

--- src/main/java/com/hourglassprograms/dungeoncraft/Main.java
+++ src/main/java/com/hourglassprograms/dungeoncraft/Main.java
@@ -1,17 +1,19 @@
 package com.hourglassprograms.dungeoncraft;
 
+import java.util.HashMap;
+
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
     @Override
     public void onEnable() {
-
+        this.saveDefaultConfig();
         getLogger().info("Dungeon Craft has loaded");
 
     }
@@ -28,8 +30,33 @@ public class Main extends JavaPlugin {
         // *Must be a player to run commands
         if (sender instanceof Player) {
             // *Test Command
-            if (label.equalsIgnoreCase("hello")) {
+            if (label.equalsIgnoreCase("dg") || label.equalsIgnoreCase("dungeoncraft")
+                    || label.equalsIgnoreCase("dc")) {
                 Player player = (Player) sender;
+                if (args.length == 0) {
+                    // Display help for dungeoncraft
+                    player.sendMessage(ChatColor.BOLD + "Unknown command. Try /dc help");
+                    return false;
+                } // *Create command - requires name for new dungeon
+                else if (args[0].equalsIgnoreCase("create-dungeon")) {
+                    if (player.hasPermission("dungeoncraft.createdungeon")) { // Must have permission
+                        if (args.length == 2) { // Makes sure correct amount of arguments
+                            player.sendMessage(ChatColor.BOLD + "Creating dungeon...");
+                            createDungeon(args[1]);
+                            // Creates new dungeon config
+
+                        } else {
+                            player.sendMessage(ChatColor.RED + "Incorrect arguments...");
+                            return false;
+                        }
+                        // Creates Dungeon
+
+                    } else {
+                        player.sendMessage(ChatColor.BOLD + "You do not have the perms to do this");
+                    }
+
+                }
+
                 if (player.hasPermission("hello.use")) {
                     player.sendMessage(ChatColor.BOLD + "Hello and welcome :P");
 
@@ -41,6 +68,26 @@ public class Main extends JavaPlugin {
             return false;
         }
         return false;
+    }
+
+    // *Creates dungeon and config
+    private void createDungeon(String name) {
+        // Sets the prefix for all of the dungeon settings
+        String prefix = "dungeons." + name + ".";
+        // Gets config
+        FileConfiguration config = this.getConfig();
+        // Number of waves to be generated
+        Integer waveCount = 5;
+        config.set(prefix + "wave-count", waveCount);
+        config.set(prefix + "dungeon-id", ((int) (Math.random() * ((1000) + 1))));
+
+        for (int i = 1; i < waveCount + 1; i++) {
+            String wavePrefix = prefix + "waves." + "wave" + i + ".zombie.";
+            config.set(wavePrefix + "health", 20);
+            config.set(wavePrefix + "amount", 5);
+            config.set(wavePrefix + "damage", 20);
+        }
+        this.saveConfig();
     }
 
 }
